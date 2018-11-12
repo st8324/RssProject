ba42d01bdf1abc66e1a9869d0d3861fb72ef26b4
Unknown <stajun@naver.com>
2018-11-13 02:17:48 +0900

    검색 기능 구현

--- src/main/java/kr/spring/project/controller/HomeController.java
+++ src/main/java/kr/spring/project/controller/HomeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import kr.spring.project.pagination.PageMaker;
 import kr.spring.project.rss.Feed;
 import kr.spring.project.rss.FeedMessage;
 import kr.spring.project.rss.RSSFeedParser;
+import kr.spring.project.service.HomeService;
 
 /**
  * Handles requests for the application home page.
@@ -22,30 +24,24 @@ import kr.spring.project.rss.RSSFeedParser;
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	@Autowired
+	HomeService homeService;
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home( Model model, Criteria cri) {
 		RSSFeedParser parser = new RSSFeedParser(
                 "https://www.now.go.kr/ur/rss/UrRssView.do?rssType=TUR_POLI_TRND&rssSubType=OVER");
 		Feed feed = parser.readFeed();
-		
-		int totalCount = feed.getMessages().size();
-		PageMaker pageMaker = new PageMaker();
-		pageMaker.setCriteria(cri);
-		pageMaker.setDisplayPageNum(10);
-		pageMaker.setTotalCount(totalCount);
+		//검색 전 게시글들
 		List<FeedMessage> feedMessages = (List)feed.getMessages();
-		feedMessages = feedMessages.subList(cri.getPageStart(),cri.getPageStart()+cri.getPerPageNum());
-		
-		for (FeedMessage message : feedMessages) {
-	        System.out.println(message);
-		}
+		//검색 후 게시글들
+		List<FeedMessage> searchMessages = homeService.getListFeedMessage(feedMessages, cri);
+		int totalCount = 0;
+		if(searchMessages != null)
+			totalCount = homeService.getCountListFeedMessage(feedMessages, cri);
+		PageMaker pageMaker = homeService.getPageMaker(cri, 10, totalCount);
 		model.addAttribute("size", totalCount);
-		model.addAttribute("feed", feedMessages);
+		model.addAttribute("feed", searchMessages);
 		model.addAttribute("pageMaker", pageMaker);
 		return "home";
 	}
